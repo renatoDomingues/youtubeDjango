3a5ab060010f521d1740add9d70da21078417282
renatoDomingues <kungfurenato30@gmail.com>
2022-09-09 15:00:48 -0300

    09_09_22

--- pessoa/urls.py
+++ pessoa/urls.py
@@ -1,12 +1,17 @@
 
 from django.urls import path
-from .views import ListaPessoaView, PessoaCreateView, PessoaUpdateView
+from .views import ListaPessoaView, PessoaCreateView, PessoaUpdateView, PessoaDeleteView
 
 urlpatterns = [
     #1° parametro da listagem, acostumamos deixar vazio, o 2° buscarmos na importação acima e o ultimo o nome da nossa rota
     path('', ListaPessoaView.as_view(), name='pessoa.index'),
     path('novo/', PessoaCreateView.as_view(), name='pessoa.novo'),
-    path('editar/<int:id>', PessoaUpdateView.as_view(), name='pessoa.editar')
+    
+    #path('editar/<int:id>', PessoaUpdateView.as_view(), name='pessoa.editar')
+    
+    path('editar/<int:pk>', PessoaUpdateView.as_view(), name='pessoa.editar'),
+    path('remover/<int:pk>', PessoaDeleteView.as_view(), name='pessoa.remover')
+    
     #Depois essa "path editar" do UPDATE, necessita chamar na pessoa_list
 ]
 
--- pessoa/views.py
+++ pessoa/views.py
@@ -2,7 +2,7 @@
 
 from django.shortcuts import render
 #Esse ListView abaixo, um beneficio Django nos fornece classe, desenvolver na tela
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Pessoa
 from .forms import PessoaForm
 
@@ -26,4 +26,8 @@ class PessoaUpdateView(UpdateView):
     success_url = '/pessoas/'
     
 #Depois criar uma URL na urls.py
+
+class PessoaDeleteView(DeleteView):
+    model = Pessoa
+    success_url = '/pessoas/'
     

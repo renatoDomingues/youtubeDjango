e9e5d796ddd7240b7ddbc3e5e2ac561bab369d77
renatoDomingues <kungfurenato30@gmail.com>
2022-09-15 16:00:15 -0300

    15_09_22

--- pessoa/forms.py
+++ pessoa/forms.py
@@ -5,6 +5,11 @@ from django.forms import fields, models
 from .models import Pessoa
 
 class PessoaForm(forms.ModelForm):
+    data_nascimento = forms.DateField(
+        widget = forms.TextInput(
+            attrs= { "type": "date" }
+        )
+    )
     class Meta:
         model = Pessoa
         fields = ['nome_completo', 'data_nascimento', 'ativa']
--- pessoa/views.py
+++ pessoa/views.py
@@ -10,6 +10,15 @@ class ListaPessoaView(ListView):
     model = Pessoa
     #trago todas dentro da Pessoa
     queryset = Pessoa.objects.all().order_by('nome_completo')
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        filtro_nome = self.request.GET.get('nome') or None
+        
+        if filtro_nome:
+            queryset = queryset.filter(nome_completo__contains = filtro_nome)
+            
+        return queryset
 
 #Depois devemos criar um urls
 
